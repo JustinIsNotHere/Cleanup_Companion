efbdcb370850fc0a577215a69b7d165af48f7b79
JustinIsNotHere <orlandojustin52@gmail.com>
2024-03-07 09:53:38 -0500

    updated conn.php to accept the up to date entries grace made for the website.

--- conn.php
+++ conn.php
@@ -1,10 +1,20 @@
 <?php
-	$ = $_POST[''];
-	$ = $_POST['']-=
-	$ = $_POST[''];
-	$ = $_POST[''];
-	$ = $_POST[''];
-	$ = $_POST[''];
+	$name = $_POST['name'];
+	$email = $_POST['email'];
+	$hometown = $_POST['hometown'];
+	$zipcode = $_POST['zipcode'];
+	$date = $_POST['date'];
+	$states = $_POST['states'];
+	$sitename = $_POST['sitename'];
+	$sitecity = $_POST['sitecity'];
+	$sitecounty = $_POST['sitecounty'];
+	$cleanuptype = $_POST['cleanuptype'];
+	$waterbottles = $_POST['waterbottles'];
+	$bottlecaps = $_POST['bottlecaps'];
+	$plasticutensils = $_POST['plasticutensils'];
+	$plasticbags = $_POST['plasticbags'];
+	$candywrappers = $_POST['candywrappers'];
+
 
 	// Database connection
 	$conn = new mysqli('localhost','root','','test');
@@ -12,12 +22,12 @@
 		echo "$conn->connect_error";
 		die("Connection Failed : ". $conn->connect_error);
 	} else {
-		$stmt = $conn->prepare("insert into registration(, , , , , ) values(?, ?, ?, ?, ?, ?)");
-		$stmt->bind_param("sssssi", $, $, $, $, $, $);
+		$stmt = $conn->prepare("Enter into registration(name, email, hometown, zipcode, date, states, sitename, sitecity, sitecounty, cleanuptype, waterbottles, bottlecaps, plasticutensils, plasticbags, candywrappers,) values(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
+		$stmt->bind_param("sssssi", $name, $email, $htown, $zip, $date, $states, $sitename, $sitecounty, $cleanuptype, $waterbottles, $bottlecaps, $plasticutensils, $plasticbags, $candywrappers);
 		$execval = $stmt->execute();
 		echo $execval;
-		echo "Registration successfully...";
+		echo "Information successfully...";
 		$stmt->close();
 		$conn->close();
 	}
-?>
+	?>
