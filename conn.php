3152a7b5f0c126ad1a335b01859335adc10de678
JustinIsNotHere <orlandojustin52@gmail.com>
2024-04-15 10:23:32 -0400

    Edited Variables

--- conn.php
+++ conn.php
@@ -1,50 +1,41 @@
 <?php
 
 	#defined the global vars, which will be used in the mysql db.
-	global $name;
-	global $email;
-	global $hometown;
-	global $zipcode;
-	global $date;
-	global $states;
-	global $sitename;
-	global $sitecity;
-	global $sitecounty;
-	global $cleanuptype;
-	global $waterbottles;
-	global $bottlecaps;
-	global $lids;
-	global $cigarettes;
-	global $ecigarettes;
-	global $plasticutensils;
-	global $plasticbags;
-	global $lighter;
-	global $hairties;
-	global $pen;
-	global $ribbon;
-	global $otherplastic;
+	global $Name;
+	global $Email;
+	global $Hometown;
+	global $Zipcode;
+	global $Date;
+	global $States;
+	global $Sitename;
+	global $Sitecity;
+	global $Sitecounty;
+	global $Cleanuptype;
+	global $Waterbottles;
+	global $Bottlecaps;
+	global $Lids;
+	global $Cigarettes;
+	global $Ecigarettes;
+	global $Plasticutensils;
+	global $Plasticbags;
+	global $Lighter;
+	global $Hairties;
+	global $Pen;
+	global $Ribbon;
 
-	global $otherplastic1;
-	global $otherplastic2;
-	global $otherplastic3;
-	global $otherplastic4;
-	global $otherplastic5;
-	global $otherplastic6;
-	global $otherplastic7;
-	global $otherplastic8;
-	global $otherplastic;
+	global $Otherplastic10a;
 
-	global $candywrappers;
-	global $caprings;
-	global $fishingline;
-	global $bait;
-	global $nets;
-	global $containers;
-	global $flosspicks;
-	global $syringes;
-	global $pstraws;
-	global $holders;
-	global $toys;
+	global $Candywrappers;
+	global $Caprings;
+	global $Fishingline;
+	global $Bait;
+	global $Nets;
+	global $Containers;
+	global $Flosspicks;
+	global $Syringes;
+	global $Pstraws;
+	global $Holders;
+	global $Toys;
 	#possible add custom amount?
 	global $batteries;
 	global $metalbottlecaps;
@@ -69,7 +60,9 @@
 	global $newspaper;
 	global $paperbags;
 	global $receipts;
+	
 	global $otherpaper;
+	
 	global $paperplates;
 	global $magazines;
 	global $paperpieces;
@@ -80,7 +73,9 @@
 	global $towels;
 	global $sticks;
 	global $wood;
+	
 	global $othermisc;
+	
 	global $balloon;
 	global $tire;
 	global $gloves;
@@ -89,27 +84,27 @@
 
 	#This stretch takes ALL variables and makes the global variables equal to the amounts entered on the form by matching id. 
 	#For ease of adding variables, all IDs should have identical names.
-	$name = $_POST['name'];
-	$email = $_POST['email'];
-	$hometown = $_POST['hometown'];
-	$zipcode = $_POST['zipcode'];
-	$date = $_POST['date'];
-	$states = $_POST['states'];
-	$sitename = $_POST['sitename'];
-	$sitecity = $_POST['sitecity'];
-	$sitecounty = $_POST['sitecounty'];
-	$cleanuptype = $_POST['cleanuptype'];
-	$waterbottles = $_POST['waterbottles'];
-	$bottlecaps = $_POST['bottlecaps'];
-	$lids = $_POST['lids'];
-	$cigarettes = $_POST['cigarettes'];
-	$ecigarettes = $_POST['ecigarettes'];
-	$plasticutensils = $_POST['plasticutensils'];
-	$plasticbags = $_POST['plasticbags'];
-	$lighter = $_POST['lighter'];
-	$hairties = $_POST['hairties'];
-	$pen = $_POST['pen'];
-	$ribbon = $_POST['ribbon'];
+	$Name = $_POST['name'];
+	$Email = $_POST['email'];
+	$Hometown = $_POST['hometown'];
+	$Zipcode = $_POST['zipcode'];
+	$Date = $_POST['date'];
+	$States = $_POST['states'];
+	$Sitename = $_POST['sitename'];
+	$Sitecity = $_POST['sitecity'];
+	$Sitecounty = $_POST['sitecounty'];
+	$Cleanuptype = $_POST['cleanuptype'];
+	$Waterbottles = $_POST['waterbottles'];
+	$Bottlecaps = $_POST['bottlecaps'];
+	$Lids = $_POST['lids'];
+	$Cigarettes = $_POST['cigarettes'];
+	$Ecigarettes = $_POST['ecigarettes'];
+	$Plasticutensils = $_POST['plasticutensils'];
+	$Plasticbags = $_POST['plasticbags'];
+	$Lighter = $_POST['lighter'];
+	$Hairties = $_POST['hairties'];
+	$Pen = $_POST['pen'];
+	$Ribbon = $_POST['ribbon'];
 	
 	$otherplastic = $_POST['customtext1'];
 	
@@ -128,7 +123,9 @@
 	$metalbottlecaps= $_POST['metalbottlecaps'];
 	$metalcans= $_POST['metalcans'];
 	$fishhooks= $_POST['fishhooks'];
-	$othermeta = $_POST['customtext2'];
+	
+	$othermetal = $_POST['customtext2'];
+	
 	$foil = $_POST['foil'];
 	$nails = $_POST['wire'];
 	$wire = $_POST['nails'];
@@ -136,7 +133,9 @@
 	$foamcup = $_POST['foamcups'];
 	$foampacking = $_POST['foampacking'];
 	$foampieces = $_POST['foampieces'];
+	
 	$otherfoam = $_POST['customtext3'];
+	
 	$foamplates = $_POST['foamplates'];
 	$foamcontainers = $_POST['foamcontainers'];
 	$trays = $_POST['trays'];
@@ -145,7 +144,9 @@
 	$newspaper = $_POST['newspaper'];
 	$paperbags = $_POST['paperbags'];
 	$receipts = $_POST['receipts'];
+	
 	$otherpaper = $_POST['customtext4'];
+	
 	$paperplates = $_POST['paperplates'];
 	$magazines = $_POST['magazines'];
 	$paperpieces = $_POST['paperpieces'];
@@ -157,13 +158,73 @@
 	$sticks= $_POST['sticks'];
 	$wood= $_POST['wood'];
 	
-	$othermisc= $_POST['customtext5'];
+	$Balloon= $_POST['balloon'];
+	$Tire= $_POST['tire'];
+	$Gloves= $_POST['gloves'];
+	$Glassbottle= $_POST['glassbottle'];
+	$Glass= $_POST['glass'];
 	
-	$balloon= $_POST['balloon'];
-	$tire= $_POST['tire'];
-	$gloves= $_POST['gloves'];
-	$glassbottle= $_POST['glassbottle'];
-	$glass= $_POST['glass'];
+	$Othermisc1 = $_POST['customtext1'];
+	$Othermisc1a = $_POST['customtext1a'];
+	$Othermisc2 = $_POST['customtext2'];
+	$Othermisc2a = $_POST['customtext2a'];
+	$Othermisc3 = $_POST['customtext3'];
+	$Othermisc3a = $_POST['customtext3a'];
+	$Othermisc4 = $_POST['customtext4'];
+	$Othermisc4a = $_POST['customtext4a'];
+	$Othermisc5 = $_POST['customtext5'];
+	$Othermisc5a = $_POST['customtext5a'];
+	$Othermisc6 = $_POST['customtext6'];
+	$Othermisc6a = $_POST['customtext6a'];
+	$Othermisc7 = $_POST['customtext7'];
+	$Othermisc7a = $_POST['customtext7a'];
+	$Othermisc8 = $_POST['customtext8'];
+	$Othermisc8a = $_POST['customtext8a'];
+	$Othermisc9 = $_POST['customtext9'];
+	$Othermisc9a = $_POST['customtext9a'];
+	$Othermisc10 = $_POST['customtext10'];
+	$Othermisc10a = $_POST['customtext10a'];
+	$Othermisc11 = $_POST['customtext11'];
+	$Othermisc11a = $_POST['customtext11a'];
+	$Othermisc12 = $_POST['customtext12'];
+	$Othermisc12a = $_POST['customtext12a'];
+	$Othermisc13 = $_POST['customtext13'];
+	$Othermisc13a = $_POST['customtext13a'];
+	$Othermisc14 = $_POST['customtext14'];
+	$Othermisc14a = $_POST['customtext14a'];
+	$Othermisc15 = $_POST['customtext15'];
+	$Othermisc15a = $_POST['customtext15a'];
+	$Othermisc16 = $_POST['customtext16'];
+	$Othermisc16a = $_POST['customtext16a'];
+	$Othermisc17 = $_POST['customtext17'];
+	$Othermisc17a = $_POST['customtext17a'];
+	$Othermisc18 = $_POST['customtext18'];
+	$Othermisc18a = $_POST['customtext18a'];
+	$Othermisc19 = $_POST['customtext19'];
+	$Othermisc19a = $_POST['customtext19a'];
+	$Othermisc20 = $_POST['customtext20'];
+	$Othermisc20a = $_POST['customtext20a'];
+	$Othermisc21 = $_POST['customtext21'];
+	$Othermisc21a = $_POST['customtext21a'];
+	$Othermisc22 = $_POST['customtext22'];
+	$Othermisc22a = $_POST['customtext22a'];
+	$Othermisc23 = $_POST['customtext23'];
+	$Othermisc23a = $_POST['customtext23a'];
+	$Othermisc24 = $_POST['customtext24'];
+	$Othermisc24a = $_POST['customtext24a'];
+	$Othermisc25 = $_POST['customtext25'];
+	$Othermisc25a = $_POST['customtext25a'];
+	$Othermisc26 = $_POST['customtext26'];
+	$Othermisc26a = $_POST['customtext26a'];
+	$Othermisc27 = $_POST['customtext27'];
+	$Othermisc27a = $_POST['customtext27a'];
+	$Othermisc28 = $_POST['customtext28'];
+	$Othermisc28a = $_POST['customtext28a'];
+	$Othermisc29 = $_POST['customtext29'];
+	$Othermisc29a = $_POST['customtext29a'];
+	$Othermisc30 = $_POST['customtext30'];
+	$Othermisc30a = $_POST['customtext30a'];
+
 
 	// Database connection
 	$conn = new mysqli('localhost','root','','test');
@@ -175,10 +236,10 @@
 		states, sitename, sitecity, sitecounty, cleanuptype, waterbottles, bottlecaps, lids, cigarettes, 
 		ecigarettes, plasticutensils, plasticbags, lighter, hairties, pen, ribbon, otherplastic, candywrappers, 
 		caprings, fishingline, bait, nets, containers, flosspicks, syringes, pstraws, holders, toys, batteries, 
-		metalbottlecaps, metalcans, fishhooks, othermetal, foil, wire, nails, poptabs, foamcups, foampacking, 
-		foampieces, otherfoam, foamplates, foamcontainers, trays, cardboard, cups, newspaper, paperbags, 
-		receipts, otherpaper, paperplates, magazines, paperstraws, napkins, clothing, shoes, towels, sticks, wood, 
-		othermisc, balloon, tire, gloves, glassbottle, glass) 
+		metalbottlecaps, metalcans, fishhooks, customtext2, foil, wire, nails, poptabs, foamcups, foampacking, 
+		foampieces, customtext3, foamplates, foamcontainers, trays, cardboard, cups, newspaper, paperbags, 
+		receipts, customtext4, paperplates, magazines, paperstraws, napkins, clothing, shoes, towels, sticks, wood, 
+		customtext5, balloon, tire, gloves, glassbottle, glass) 
 		values(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?,)");
 		
 		$stmt->bind_param("sssssi", $name, $email, $htown, $zip, $date, $states, $sitename, $sitecounty, 
@@ -186,7 +247,11 @@
 		$plasticbags, $lighter, $hairties, $pen, $ribbon, $otherplastic, $candywrappers, $caprings, 
 		$fishingline, $bait, $nets, $containers, $flosspicks, $syringes, $pstraws, $holders, $toys, 
 		$batteries, $metalbottlecaps, $metalcans, $fishhooks, $othermetal, $foil, $wire, $nails, 
-		$poptabs, $foamcups, $foampacking, $foampieces, $otherfoam, $foamplates, $foamcontainers, $trays, $cardboard, $cups, $newspaper, $paperbags, $receipts,);
+		$poptabs, $foamcups, $foampacking, $foampieces, $otherfoam, $foamplates, $foamcontainers, 
+		$trays, $cardboard, $cups, $newspaper, $paperbags, $receipts,$otherpaper, $paperplates,
+		$magazines, $paperpieces, $paperstraws, $napkins, $clothing, $shoes, $towels, $sticks, 
+		$wood, $othermisc, $balloon, $tire, $gloves, $glassbottle, $glass);
+		
 		$execval = $stmt->execute();
 		
 		echo $execval;
