99f34bb9811cb9ec3fe5be586281d97bc3b706e5
JustinIsNotHere <orlandojustin52@gmail.com>
2024-05-06 09:26:21 -0400

    Fixed up code for connection.

--- conn.php
+++ conn.php
@@ -22,9 +22,6 @@
 	global $Hairties;
 	global $Pen;
 	global $Ribbon;
-
-	global $Otherplastic10a;
-
 	global $Candywrappers;
 	global $Caprings;
 	global $Fishingline;
@@ -36,8 +33,6 @@
 	global $Pstraws;
 	global $Holders;
 	global $Toys;
-<<<<<<< HEAD
-	#possible add custom amount?
 	global $batteries;
 	global $metalbottlecaps;
 	global $metalcans;
@@ -50,9 +45,6 @@
 	global $foamcup;
 	global $foampacking;
 	global $foampieces;
-	
-	global $otherfoam;
-
 	global $foamplates;
 	global $foamcontainers;
 	global $trays;
@@ -61,7 +53,6 @@
 	global $newspaper;
 	global $paperbags;
 	global $receipts;
-=======
 	global $Batteries;
 	global $Metalbottlecaps;
 	global $Metalcans;
@@ -93,28 +84,73 @@
 	global $Towels;
 	global $Sticks;
 	global $Wood;
->>>>>>> 3af776d (names for Vars fully finalized, code essentially)
 	
-	global $otherpaper;
+	global $Othermisc1;
+	global $Othermisc1a;
+	global $Othermisc2;
+	global $Othermisc2a;
+	global $Othermisc3;
+	global $Othermisc3a;
+	global $Othermisc4;
+	global $Othermisc4a;
+	global $Othermisc5;
+	global $Othermisc5a;
+	global $Othermisc6;
+	global $Othermisc6a;
+	global $Othermisc7;
+	global $Othermisc7a;
+	global $Othermisc8;
+	global $Othermisc8a;
+	global $Othermisc9;
+	global $Othermisc9a;
+	global $Othermisc10;
+	global $Othermisc10a;
+	global $Othermisc11;
+	global $Othermisc11a;
+	global $Othermisc12;
+	global $Othermisc12a;
+	global $Othermisc13;
+	global $Othermisc13a;
+	global $Othermisc14;
+	global $Othermisc14a;
+	global $Othermisc15;
+	global $Othermisc15a;
+	global $Othermisc16;
+	global $Othermisc16a;
+	global $Othermisc17;
+	global $Othermisc17a;
+	global $Othermisc18;
+	global $Othermisc18a;
+	global $Othermisc19;
+	global $Othermisc19a;
+	global $Othermisc20;
+	global $Othermisc20a;
+	global $Othermisc21;
+	global $Othermisc21a;
+	global $Othermisc22;
+	global $Othermisc22a;
+	global $Othermisc23;
+	global $Othermisc23a;
+	global $Othermisc24;
+	global $Othermisc24a;
+	global $Othermisc25;
+	global $Othermisc25a;
+	global $Othermisc26;
+	global $Othermisc26a;
+	global $Othermisc27;
+	global $Othermisc27a;
+	global $Othermisc28;
+	global $Othermisc28a;
+	global $Othermisc29;
+	global $Othermisc29a;
+	global $Othermisc30;
+	global $Othermisc30a;
 	
-	global $paperplates;
-	global $magazines;
-	global $paperpieces;w
-	global $paperstraws;
-	global $napkins;
-	global $clothing;
-	global $shoes;
-	global $towels;
-	global $sticks;
-	global $wood;
-	
-	global $othermisc;
-	
-	global $balloon;
-	global $tire;
-	global $gloves;
-	global $glassbottle;
-	global $glass;
+	global $Balloon;
+	global $Tire;
+	global $Gloves;
+	global $Glassbottle;
+	global $Glass;
 
 	#This stretch takes ALL variables and makes the global variables equal to the amounts entered on the form by matching id. 
 	#For ease of adding variables, all IDs should have identical names.
@@ -139,9 +175,6 @@
 	$Hairties = $_POST['hairties'];
 	$Pen = $_POST['pen'];
 	$Ribbon = $_POST['ribbon'];
-	
-	$otherplastic = $_POST['customtext1'];
-	
 	$candywrappers = $_POST['candywrappers'];
 	$caprings = $_POST['caprings'];
 	$fishingline = $_POST['fishingline'];
@@ -156,10 +189,7 @@
 	$batteries = $_POST['batteries'];
 	$metalbottlecaps= $_POST['metalbottlecaps'];
 	$metalcans= $_POST['metalcans'];
-	$fishhooks= $_POST['fishhooks'];
-	
-	$othermetal = $_POST['customtext2'];
-	
+	$fishhooks= $_POST['fishhooks'];	
 	$foil = $_POST['foil'];
 	$nails = $_POST['wire'];
 	$wire = $_POST['nails'];
@@ -167,8 +197,6 @@
 	$foamcup = $_POST['foamcups'];
 	$foampacking = $_POST['foampacking'];
 	$foampieces = $_POST['foampieces'];
-	
-	$otherfoam = $_POST['customtext3'];
 	
 	$foamplates = $_POST['foamplates'];
 	$foamcontainers = $_POST['foamcontainers'];
@@ -178,8 +206,6 @@
 	$newspaper = $_POST['newspaper'];
 	$paperbags = $_POST['paperbags'];
 	$receipts = $_POST['receipts'];
-	
-	$otherpaper = $_POST['customtext4'];
 	
 	$paperplates = $_POST['paperplates'];
 	$magazines = $_POST['magazines'];
@@ -266,7 +292,7 @@
 		echo "$conn->connect_error";
 		die("Connection Failed : ". $conn->connect_error);
 	} else {
-<<<<<<< HEAD
+
 		$stmt = $conn->prepare("Enter into the form if applicable (name, email, hometown, zipcode, date, 
 		states, sitename, sitecity, sitecounty, cleanuptype, waterbottles, bottlecaps, lids, cigarettes, 
 		ecigarettes, plasticutensils, plasticbags, lighter, hairties, pen, ribbon, otherplastic, candywrappers, 
@@ -286,12 +312,10 @@
 		$trays, $cardboard, $cups, $newspaper, $paperbags, $receipts,$otherpaper, $paperplates,
 		$magazines, $paperpieces, $paperstraws, $napkins, $clothing, $shoes, $towels, $sticks, 
 		$wood, $othermisc, $balloon, $tire, $gloves, $glassbottle, $glass);
-=======
 		$stmt = $conn->prepare("Enter into the form if applicable (name,email,htown,zip,date,states,sitename,sitecounty,cleanuptype,waterbottles,bottlecaps,lids,cigarettes,ecigarettes,plasticutensils,plasticbags,lighter,hairties,pen,ribbon,candywrappers,caprings,fishingline,bait,nets,containers,flosspicks,syringes,pstraws,holders,toys,batteries,metalbottlecaps,netalcans,fishhooks,foil,wire,nails,poptabs,foamcups,foampacking,foampieces,foamplates,foamcontainers,trays,cardboard,cups,newspaper,paperbags,receipts,paperplates,magazines,paperpieces,paperstraws,napkins,clothing,shoes,towels,sticks,wood,balloon,tire,gloves,glassbottle,glass,customtext1,customtext1a,customtext2,customtext2a,customtext3,customtext3a,customtext4,customtext4a,customtext5,customtext5a,customtext6,customtext6a,customtext7,customtext7a,customtext8,customtext8a,customtext9,customtext9a,customtext10,customtext10a,customtext11,customtext11a,customtext12,customtext12a,customtext13,customtext13a,customtext14,customtext14a,customtext15,customtext15a,customtext16,customtext16a,customtext17,customtext17a,customtext18,customtext18a,customtext19,customtext19a,customtext20,customtext20a,customtext21,customtext21a,customtext22,customtext22a,customtext23,customtext23a,customtext24,customtext24a,customtext25,customtext25a,customtext26,customtext26a,customtext27,customtext27a,customtext28,customtext28a,customtext29,customtext29a,customtext30,customtext30a) 
 		values(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)");
 		
 		$stmt->bind_param("sssssi", $Name, $Email, $Htown, $Zip, $Date, $States, $Sitename, $Sitecounty, $Cleanuptype, $Waterbottles, $Bottlecaps, $Lids, $Cigarettes, $Ecigarettes, $Plasticutensils, $Plasticbags, $Lighter, $Hairties, $Pen, $Ribbon, $Candywrappers, $Caprings, $Fishingline, $Bait, $Nets, $Containers, $Flosspicks, $Syringes, $Pstraws, $Holders, $Toys, $Batteries, $Metalbottlecaps, $Metalcans, $Fishhooks, $Foil, $Wire, $Nails, $Poptabs, $Foamcups, $Foampacking, $Foampieces, $Foamplates, $Foamcontainers, $Trays, $Cardboard, $Cups, $Newspaper, $Paperbags, $Receipts, $Paperplates,$Magazines, $Paperpieces, $Paperstraws, $Napkins, $Clothing, $Shoes, $Towels, $Sticks, $Wood, $Balloon, $Tire, $Gloves, $Glassbottle, $Glass, $Othermisc1,$Othermisc1a,$Othermisc2,$Othermisc2a,$Othermisc3,$Othermisc3a,$Othermisc4,$Othermisc4a,$Othermisc5,$Othermisc5a,$Othermisc6,$Othermisc6a,$Othermisc7,$Othermisc7a,$Othermisc8,$Othermisc8a,$Othermisc9,$Othermisc9a,$Othermisc10,$Othermisc10a, $Othermisc11, $Othermisc11a,$Othermisc12,$Othermisc12a,$Othermisc13,$Othermisc13a,$Othermisc14,$Othermisc14a,$Othermisc15,$Othermisc15a,$Othermisc16,$Othermisc16a,$Othermisc17,$Othermisc17a,$Othermisc18,$Othermisc18a,$Othermisc19,$Othermisc19a,$Othermisc20,$Othermisc20a,$Othermisc21,$Othermisc21a,$Othermisc22,$Othermisc22a,$Othermisc23,$Othermisc23a,$Othermisc24,$Othermisc24a,$Othermisc25,$Othermisc25a,$Othermisc26,$Othermisc26a,$Othermisc27,$Othermisc27a,$Othermisc28,$Othermisc28a,$Othermisc29,$Othermisc29a,$Othermisc30,$Othermisc30a);
->>>>>>> 3af776d (names for Vars fully finalized, code essentially)
 		
 		$execval = $stmt->execute();
 		
